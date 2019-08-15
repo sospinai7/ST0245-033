cb366eac626f4ae26401b299ab727508d1d49fbf
sospinai7 <53051500+sospinai7@users.noreply.github.com>
2019-08-15 11:56:07 -0500

    Create ejercicio3.java

---
+++ talleres/taller02/ejercicio3.java
@@ -0,0 +1,15 @@
+public class ejercicio3 {
+	
+	public static void combinations(String s) { 
+		combinationsAux("", s); 
+	}
+
+	 private static void combinationsAux(String prefix, String s) {
+      	if(s.length() == 0) {
+	         System.out.println( prefix );
+	      }else{
+	         combinationsAux( prefix + s.charAt(0), s.substring(1));
+	         combinationsAux( prefix, s.substring( 1 ));
+	}
+
+}
