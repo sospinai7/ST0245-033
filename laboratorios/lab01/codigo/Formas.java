b4e437f026df9914843a6a656be800d2e6f0a2a9
sospinai7 <53051500+sospinai7@users.noreply.github.com>
2019-08-18 17:09:31 -0500

    Update and rename Rectangulos.java to Formas.java

---
+++ laboratorios/lab01/codigo/Formas.java
@@ -0,0 +1,6 @@
+public class Formas{ 
+       public int formas(int n){ 
+                  if(n <= 2)  n; 
+                  return formas(n-1) +                                   
+                        formas(n-2); 
+}
