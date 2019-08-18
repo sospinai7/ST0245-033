b1b71d91abb17cc41c9258c8c3a747d13a7d7903
sospinai7 <53051500+sospinai7@users.noreply.github.com>
2019-08-18 00:40:22 -0500

    Create Rectangulos.java

---
+++ laboratorios/lab01/codigo/Rectangulos.java
@@ -0,0 +1,9 @@
+public class Rectangulos{ 
+        public static boolean groupSum(int start, int[] nums, int target){
+                if (start == nums.length){
+                  return target == 0; 
+                }else{
+                  return groupSum(start+1,nums, target - nums[start]) || groupSum(start+1,nums, target);  
+                }
+        }
+}
