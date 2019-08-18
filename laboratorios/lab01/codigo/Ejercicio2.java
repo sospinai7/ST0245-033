75ca52bf8a43a14423e54440bcf9041bf92be105
sospinai7 <53051500+sospinai7@users.noreply.github.com>
2019-08-18 00:40:52 -0500

    Delete Ejercicio2.java

--- laboratorios/lab01/codigo/Ejercicio2.java
+++
@@ -1,9 +0,0 @@
-public class Ejercicio2{ 
-        public static boolean groupSum(int start, int[] nums, int target){
-                if (start == nums.length){
-                  return target == 0; 
-                }else{
-                  return groupSum(start+1,nums, target - nums[start]) || groupSum(start+1,nums, target);  
-                }
-        }
-}
